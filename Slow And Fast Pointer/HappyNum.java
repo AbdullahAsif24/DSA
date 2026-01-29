c821137927aa75bd36d24f3c4683b0ec210dc250
AbdullahAsif24 <abdullahasif4045@gmail.com>
2026-01-29 19:37:39 +0500

    .

---
+++ Slow And Fast Pointer/HappyNum.java
@@ -0,0 +1,46 @@
+/*
+Write an algorithm to determine if a number n is happy.
+
+A happy number is a number defined by the following process:
+
+Starting with any positive integer, replace the number by the sum of the squares of its digits.
+Repeat the process until the number equals 1 (where it will stay), or it loops endlessly in a cycle which does not include 1.
+Those numbers for which this process ends in 1 are happy.
+Return true if n is a happy number, and false if not.
+*/
+
+public class HappyNum {
+    public static void main(String[] args) {
+        
+    }
+
+    public static boolean isHappy(int n) {
+        int slow = n;
+        int fast = n;
+
+        while (fast != 1) {
+            slow = fun(slow);
+            fast = fun(fun(fast));
+
+            if(slow == fast && slow != 1){
+                return false;
+            }
+        }
+
+
+        return true;
+    }
+
+    public static int fun(int n){
+        int sum = 0;
+
+        while (n > 0) {
+            int d = n % 10;
+            n = n / 10;
+
+            sum += d*d;
+        }
+
+        return sum;
+    }
+}
