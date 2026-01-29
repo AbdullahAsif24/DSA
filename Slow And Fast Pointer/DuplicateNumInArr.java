21dfba8596adaa70c38698d8c76ca54219382669
AbdullahAsif24 <abdullahasif4045@gmail.com>
2026-01-29 19:16:42 +0500

    .

---
+++ Slow And Fast Pointer/DuplicateNumInArr.java
@@ -0,0 +1,38 @@
+/*
+Given an array of integers nums containing n + 1 integers where each integer is in the range [1, n] inclusive.
+
+There is only one repeated number in nums, return this repeated number.
+
+You must solve the problem without modifying the array nums and using only constant extra space.
+
+ 
+*/
+
+public class DuplicateNumInArr {
+    public static void main(String[] args) {
+        
+    }
+
+    public static int findDuplicate(int[] nums) {
+        int slow = nums[0];
+        int fast = nums[0];
+
+        // Find intersection inside cycle
+        do {
+            slow = nums[slow];
+            fast = nums[nums[fast]];
+        } while (slow != fast);
+
+        // Find entrance to cycle (duplicate)
+        slow = nums[0];
+        while (slow != fast) {
+            slow = nums[slow];
+            fast = nums[fast];
+        }
+
+        return slow;
+
+    }
+
+
+}
